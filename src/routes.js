9867b93e3a89f0f8e939d5f06a15e81c137496d7
vivian_xu <540978449@qq.com>
2016-11-29 22:40:39 +0800

    增加新页面, test 页面的 路由

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,16 @@
 import { Route, IndexRoute } from 'react-router';
 
+import Test from './pages/Test';
 import App from './pages/app/App';
 import ArticleListPage from './pages/articleListBlock/ArticleListPage';
-// import ArticleListBlock from './pages/articleListBlock/ArticleListBlock';
 import Profile from './pages/profile/Profile';
 import Article from './pages/article/Article';
 import AllComments from './pages/allComments/AllComments';
 import Comments from './pages/allComments/Comments';
-import InfiniteLoadScroll from './components/infiniteLoadScroll/InfiniteLoadScroll';
+// import InfiniteLoadScroll from './components/infiniteLoadScroll/InfiniteLoadScroll';
+import Questions from './pages/questions/Questions';
+import About from './pages/about/About';
+import Deal from './pages/deal/Deal';
 
 const routes = (
   <Route path='/' component={App}>
@@ -16,6 +19,10 @@ const routes = (
     <Route path="/articles/:articleid" component={ Article } />
     <Route path="/mentors/:mentorId" component={ Profile } />
     <Route path="/mentors/:mentorId/comments" component={ AllComments } />
+    <Route path="/test" component={ Test } />
+    <Route path="/qa" component={ Questions } />
+    <Route path="/about" component={ About } />
+    <Route path="/deal" component={ Deal } />
   </Route>
 );
 
